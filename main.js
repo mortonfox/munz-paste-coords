0c2add259b699ccb848a6ff9ad29e7dd4a08d6d6
Morton Fox <github@qslw.com>
2022-08-25 20:24:05 -0400

    Button hover effect

--- main.js
+++ main.js
@@ -4,18 +4,12 @@ function init() {
   'use strict';
 
   function setupElems() {
-    let btn = document.createElement('button');
-    btn.name = btn.id = 'paste_coords';
-    btn.style.cssText = 'background-color: #E82A24; color: #fff; font-weight: 700; border: none; padding: 6px 10px; cursor: pointer; margin: 0; display: block';
-    btn.appendChild(document.createTextNode('Paste Coords'));
-    btn.addEventListener('click', eventPasteCoords);
-
     let status_mesg = document.createElement('div');
     status_mesg.name = status_mesg.id = 'paste_coords_status_mesg';
     status_mesg.style.display = 'none';
 
     let fragment = new DocumentFragment();
-    fragment.appendChild(btn);
+    fragment.appendChild(pasteButton());
     fragment.appendChild(status_mesg);
 
     let mainarea = document.getElementsByClassName('munzee-main-area');
@@ -31,28 +25,48 @@ function init() {
   function setStatusOk(text) { setStatus(text, false); }
   function setStatusError(text) { setStatus(text, true); }
 
-  async function eventPasteCoords(event) {
-    event.preventDefault();
+  function pasteButton() {
+    let btn = document.createElement('button');
+    btn.name = btn.id = 'paste_coords';
+    btn.style.cssText = 'background-color: #fff; color: #E82A24; font-weight: 700; border: solid #E82A24; padding: 6px 10px; cursor: pointer; margin: 0; display: block';
+    btn.appendChild(document.createTextNode('Paste Coords'));
 
-    try {
-      let text = await navigator.clipboard.readText();
-      setStatusOk('Clipboard: ' + text);
-
-      let coords = text.match(/-?\d+(?:\.\d+)?/g);
-      if (coords === null || coords.length < 2) {
-        setStatusError('Coordinates not found in clipboard');
+    btn.addEventListener('mouseenter',
+      () => {
+        btn.style.color = '#fff';
+        btn.style.backgroundColor = '#E82A24';
       }
-      else {
-        setStatusOk('Fetched coordinates from clipboard');
-
-        let latitude = document.getElementById('latitude');
-        latitude.value = coords[0];
-        let longitude = document.getElementById('longitude');
-        longitude.value = coords[1];
+    );
+    btn.addEventListener('mouseleave',
+      () => {
+        btn.style.color = '#E82A24';
+        btn.style.backgroundColor = '#fff';
       }
-    } catch (e) {
-      setStatusError('Failed to read clipboard: ' + err);
-    }
+    );
+
+    btn.addEventListener('click', 
+      async (event) => {
+        event.preventDefault();
+        try {
+          let text = await navigator.clipboard.readText();
+          setStatusOk('Clipboard: ' + text);
+
+          let coords = text.match(/-?\d+(?:\.\d+)?/g);
+          if (coords === null || coords.length < 2) {
+            setStatusError('Coordinates not found in clipboard');
+          }
+          else {
+            document.getElementById('latitude').value = coords[0];
+            document.getElementById('longitude').value = coords[1];
+            setStatusOk('Fetched coordinates from clipboard');
+          }
+        } catch (e) {
+          setStatusError('Failed to read clipboard: ' + err);
+        }
+      }
+    );
+
+    return btn;
   }
 
   setupElems();
